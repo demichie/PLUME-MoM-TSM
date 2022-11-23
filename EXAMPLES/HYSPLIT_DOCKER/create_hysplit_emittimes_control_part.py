27068d704e622775125318077d31b7e7307be882
federicapardini <federica.pardini@ingv.it>
2022-11-23 12:19:11 +0100

    Update create hysplit_emittimes_control_part.py

--- EXAMPLES/HYSPLIT_DOCKER/create_hysplit_emittimes_control_part.py
+++ EXAMPLES/HYSPLIT_DOCKER/create_hysplit_emittimes_control_part.py
@@ -108,7 +108,8 @@ maximum = max(dict_h, key=dict_h.get)
 num_occurrence = int(dict_h[maximum]) # count the number of plumemom runs within each hour
 
 # particle diameters phi scale
-diam_phi = np.linspace(phi_min+(delta_phi),phi_min+(delta_phi)+((n_sections-1)*delta_phi),n_sections, endpoint=True)
+diam_phi = np.linspace(phi_min+(delta_phi),phi_min+(delta_phi)+((n_sections-1)*delta_phi),n_sections, endpoint=True) - 0.5*delta_phi
+print("diam_phi ",diam_phi)
 
 # diameter in millimeters [mm]
 diam = 2.0**(-np.asarray(diam_phi))
